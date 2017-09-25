babf5375a28d8e544ef06da27a58d84f4d0b3eb0
mu <hmu332233@gmail.com>
2017-09-26 01:13:06 +0900

    [추가] retify model
    retify/CandWordList.java
    retify/Error.java
    retify/Help.java
    retify/PnuErrorWord.java
    retify/PnuErrorWordList.java
    retify/PnuNlpSpeller.java

---
+++ app/src/main/java/com/blackdog/dictation_teacher/models/retify/PnuNlpSpeller.java
@@ -0,0 +1,23 @@
+package com.blackdog.dictation_teacher.models.retify;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class PnuNlpSpeller
+{
+    @SerializedName("pnuErrorWordList")
+    @Expose
+    private PnuErrorWordList[] pnuErrorWordList;
+
+    public PnuErrorWordList[] getPnuErrorWordList ()
+    {
+        return pnuErrorWordList;
+    }
+
+    public void setPnuErrorWordList (PnuErrorWordList[] PnuErrorWordList)
+    {
+        this.pnuErrorWordList = PnuErrorWordList;
+    }
+
+}
