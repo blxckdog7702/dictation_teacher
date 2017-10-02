05440358b471f99975aa669db7174291d537421b
minung.han <hmu332233@gmail.com>
2017-10-02 23:54:53 +0900

    [추가] delete matching

--- app/src/main/java/com/blackdog/dictation_teacher/net/ApiRequester.java
+++ app/src/main/java/com/blackdog/dictation_teacher/net/ApiRequester.java
@@ -245,4 +245,15 @@ public class ApiRequester {
 		Call<List<Student>> call = dictationServerApi.getTeachersApplicants(teacherLoginID);
 		call.enqueue(new ObjectCallback<>(userCallback));
 	}
+  //등록된 선생님 목록보기
+	public void getStudentsTeachers(String studentID, UserCallback<List<Teacher>> userCallback){
+		Call<List<Teacher>> call = dictationServerApi.getStudentsTeachers(studentID);
+		call.enqueue(new ObjectCallback<>(userCallback));
+	}
+	//매칭 끊기
+	public void unConnectedMatching(String studentID, String teacherID, UserCallback<Boolean> userCallback){
+		Call<okhttp3.ResponseBody> call = dictationServerApi.unConnectedMatching(studentID, teacherID);
+		call.enqueue(new ResultCallback(userCallback));
+	}
+
 }
