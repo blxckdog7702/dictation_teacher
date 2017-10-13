bbcb2d22ebecdb62e0f0ccf82e998cf9aea05679
DH <dohun94@naver.com>
2017-10-13 20:00:40 +0900

    [추가]학생 목록

--- app/src/main/java/com/blackdog/dictation_teacher/Activity/StudentListActivity.java
+++ app/src/main/java/com/blackdog/dictation_teacher/Activity/StudentListActivity.java
@@ -1,11 +1,21 @@
 package com.blackdog.dictation_teacher.Activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.blackdog.dictation_teacher.Activity.base.BaseDrawerActivity;
+import com.blackdog.dictation_teacher.Adapter.StudentListAdapter;
 import com.blackdog.dictation_teacher.R;
+import com.blackdog.dictation_teacher.models.Student;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentListActivity extends BaseDrawerActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +23,32 @@ public class StudentListActivity extends BaseDrawerActivity {
         setContentView(R.layout.activity_student_list);
 
         toolbarTitle.setText("학생목록");
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.student_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        List<Student> dataset = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            Student item = new Student();
+            item.setSchool("호성초등학교");
+            item.setGrade("1학년");
+            item.setClass_("2반");
+            item.setStudentId(1);
+            item.setName("홍길동");
+
+            dataset.add(item);
+        }
+
+        // specify an adapter (see also next example)
+        mAdapter = new StudentListAdapter(this, dataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
