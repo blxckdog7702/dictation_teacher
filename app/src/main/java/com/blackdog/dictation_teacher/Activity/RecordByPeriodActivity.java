6d9f5862531929bd427a38456e51265bf74450e6
DH <dohun94@naver.com>
2017-10-15 00:31:05 +0900

    [추가] RecordByPeriod Activity

---
+++ app/src/main/java/com/blackdog/dictation_teacher/Activity/RecordByPeriodActivity.java
@@ -0,0 +1,17 @@
+package com.blackdog.dictation_teacher.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.blackdog.dictation_teacher.R;
+
+public class RecordByPeriodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_by_period);
+
+
+    }
+}
